71428138789c156c155670a610413a4cd346f52a
Nikkil Kumar <nikkil@Nikkils-MacBook-Air.local>
2023-12-26 00:15:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Parallex from './components/Parallex'
+
+function App() {
+
+
+  return (
+   <Parallex />
+  )
+}
+
+export default App
---
+++ src/components/Parallex.jsx
@@ -0,0 +1,54 @@
+const Parallex = () => {
+  return (
+    <div className='container'>
+       
+        <div className="parallex-container">
+
+        <div className="parallex-scroller">
+            <div className="item  ">
+            <div className="item-1" draggable="false" ></div>
+            </div>
+            <div className="item ">
+            <div className="item-2"></div>
+            </div>
+            <div className="item ">
+            <div className="item-3"></div>
+            </div>
+            <div className="item ">
+            <div className="item-4"></div>
+            </div>
+            <div className="item ">
+            <div className="item-5"></div>
+            </div>
+            <div className="item ">
+            <div className="item-6"></div>
+            </div>
+            <div className="item ">
+            <div className="item-7"></div>
+            </div>
+            <div className="item ">
+            <div className="item-8"></div>
+            </div>
+            <div className="item ">
+            <div className="item-9"></div>
+            </div>
+            <div className="item ">
+            <div className="item-10"></div>
+            </div>
+            <div className="item ">
+            <div className="item-11"></div>
+            </div>
+            <div className="item ">
+            <div className="item-12"></div>
+            </div>
+            <div className="item ">
+            <div className="item-13"></div>
+            </div>
+        {/* hello */}
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Parallex
