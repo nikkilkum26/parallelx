02f7c0b7439419055ef009d5e03a00f0c57a0834
Nikkil Kumar <nikkil@Nikkils-MacBook-Air.local>
2023-12-26 19:54:00 +0530

    improvements, optimization and dynamic manner is implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import './App.css'
-import Parallex from './components/Parallex'
+import { Suspense, lazy } from 'react';
+const Parallex = lazy(() => import('./components/Parallex'));
+// const pic_1= lazy(() => import('./assets/pic_1.jpg'));
+import pic_1 from './assets/pic_1.jpg'
+import pic_2 from './assets/pic_2.jpg'
+import pic_3 from './assets/pic_3.jpg'
+import pic_4 from './assets/pic_4.jpg'
+import pic_5 from './assets/pic_5.jpg'
+import pic_6 from './assets/pic_6.jpg'
+import pic_7 from './assets/pic_7.jpg'
+import pic_8 from './assets/pic_8.jpg'
+import pic_9 from './assets/pic_9.jpg'
+
 
 function App() {
 
 
   return (
-   <Parallex />
-  )
+    <Suspense fallback={"Loading ..."}>
+
+    <Parallex
+      selectedImages={[
+        pic_1,
+        pic_2,
+        pic_3,
+        pic_4,
+        pic_5,
+        pic_6,
+        pic_7,
+        pic_8,
+        pic_9,
+        // pic_10,
+        // pic_11,
+        // pic_12,
+      ]}
+    />
+     </Suspense>
+  );
 }
 
 export default App
--- src/components/Parallex.jsx
+++ src/components/Parallex.jsx
@@ -1,54 +1,59 @@
-const Parallex = () => {
+import { useRef, useState } from "react";
+import { useEffect } from "react";
+
+const Parallex = ({ selectedImages }) => {
+  const picRef = useRef([]);
+  const parallelXScroller = useRef("");
+  const [imageList, setImageList] = useState([]);
+
+  useEffect(() => {
+    if (!selectedImages.length) return;
+    setImageList([...selectedImages]);
+    if (parallelXScroller.current) {
+      let len = (selectedImages.length || 0) + 2;
+      // parallelXScroller.current[0]?.style.height = `repeat(12,300px)`
+      console.log(
+        (parallelXScroller.current.style.gridTemplateColumns = `repeat(${len},300px)`)
+      );
+    }
+  }, [selectedImages]);
+
+  useEffect(() => {
+    if (picRef.current?.length) {
+      imageList.forEach((eachList, idx) => {
+        picRef.current[idx].style.backgroundImage = `url(${eachList})`;
+      });
+    }
+  }, [picRef.current, imageList]);
+
   return (
-    <div className='container'>
-       
-        <div className="parallex-container">
-
-        <div className="parallex-scroller">
-            <div className="item  ">
+    <div className="container">
+      <div className="parallex-container">
+        <div className="parallex-scroller" ref={parallelXScroller}>
+          <div className="item  ">
             {/* <div className="item-1" draggable="false" ></div> */}
-            </div>
-            <div className="item ">
-            <div className="item-2"></div>
-            </div>
-            <div className="item ">
-            <div className="item-3"></div>
-            </div>
-            <div className="item ">
-            <div className="item-4"></div>
-            </div>
-            <div className="item ">
-            <div className="item-5"></div>
-            </div>
-            <div className="item ">
-            <div className="item-6"></div>
-            </div>
-            <div className="item ">
-            <div className="item-7"></div>
-            </div>
-            <div className="item ">
-            <div className="item-8"></div>
-            </div>
-            <div className="item ">
-            <div className="item-9"></div>
-            </div>
-            <div className="item ">
-            {/* <div className="item-10"></div> */}
-            </div>
-            <div className="item ">
-            {/* <div className="item-11"></div> */}
-            </div>
-            <div className="item ">
-            {/* <div className="item-12"></div> */}
-            </div>
-            <div className="item ">
-            {/* <div className="item-13"></div> */}
-            </div>
-        {/* hello */}
-        </div>
-        </div>
-    </div>
-  )
-}
+          </div>
 
-export default Parallex
+          {imageList.map((eachImg, idx) => (
+            <div className="item " key={eachImg}>
+              <div
+                className="common"
+                ref={(element) => {
+                  picRef.current[idx] = element;
+                }}
+              ></div>
+            </div>
+          ))}
+
+          <div className="item ">{/* <div className="item-11"></div> */}</div>
+          <div className="item ">{/* <div className="item-12"></div> */}</div>
+          <div className="item ">{/* <div className="item-13"></div> */}</div>
+          <div className="item ">{/* <div className="item-13"></div> */}</div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Parallex;
